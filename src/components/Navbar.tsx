5af8d7e7dc4479006e69db22c3e8937341a7c9f3
Tyasweningas <tyasweningas@gmail.com>
2025-09-26 22:46:14 +0700

    modified Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,38 +1,42 @@
+import Link from "next/link";
+import Image from "next/image";
 const Navbar = () => {
     return (
         <nav className="bg-white">
     <div className="flex items-center justify-between w-full max-w-[1130px] py-[22px] mx-auto">
-      <a href="index.html">
-        <img src="/assets/images/logos/logo.svg" alt="logo" />
-      </a>
+      <Link href="">
+        <Image width={165} height={36} src="/assets/images/logos/logo.svg" alt="logo" />
+      </Link>
       <ul className="flex items-center gap-[50px] w-fit">
         <li>
-          <a href="">Browse</a>
+          <Link href="/">Browse</Link>
         </li>
         <li>
-          <a href="">Popular</a>
+          <Link href="/">Popular</Link>
         </li>
         <li>
-          <a href="">Categories</a>
+          <Link href="/">Categories</Link>
         </li>
         <li>
-          <a href="">Events</a>
+          <Link href="/">Events</Link>
         </li>
         <li>
-          <a href="view-booking-details.html">My Booking</a>
+          <Link href="/">My Booking</Link>
         </li>
       </ul>
-      <a
-        href="#"
+      <Link
+        href="/"
         className="flex items-center gap-[10px] rounded-full border border-[#000929] py-3 px-5"
       >
-        <img
+        <Image
           src="/assets/images/icons/call.svg"
           className="w-6 h-6"
+          width={24}
+          height={24}
           alt="icon"
         />
         <span className="font-semibold">Contact Us</span>
-      </a>
+      </Link>
     </div>
   </nav>
     );
