a061261ed0e894ad07b2f82672b47a827531e4b0
Tyasweningas <tyasweningas@gmail.com>
2025-09-30 22:52:28 +0700

    Add cities features

---
+++ src/features/cities/data/cities.mock.ts
@@ -0,0 +1,27 @@
+import { City } from "../types/city.types";
+
+const cityNames = [
+    "Jakkarta Pusat",
+    "Jakarta Selatan",
+    "Bandung",
+    "Surabaya",
+    "Yogyakarta",
+    "Medan",
+    "Semarang",
+    "Makassar",
+    "Bekasi",
+    "Depok"
+];
+
+export const cities: City[] = cityNames.map((cityName, i) => { 
+    // const officeCount = 
+
+    return {
+        id: 1 + i,
+        cityName,
+        officeCount: 1,
+        image: `assets/images/thumbnails/thumbnails-${(i & 3) + 1}.png`,
+        slug: cityName.toLowerCase().replace(/ /g, "-")
+    }
+});
+
---
+++ src/features/cities/types/city.types.ts
@@ -0,0 +1,8 @@
+export type City = {
+    id: number;
+    name: string;
+    officeCount: number;
+    image: string;
+    slug: string;
+
+}
